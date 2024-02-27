fc33c9c0f45d159a5b6639ab48519f167eb0e579
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-02-27 10:25:33 -0300

    menu responsivo adcionado e cores alteradas para as da logo

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+import ListagemProdutos from '@/components/ListagemProdutos.vue';
+import MenuSuperior from '@/components/MenuSuperior.vue';
+</script>
+
 <template>
-  <span>Em breve</span>
+  <MenuSuperior />
+  <ListagemProdutos />
 </template>
