0e717cb2cbdd9375eb66424f3f8cff480881f481
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-03-25 11:52:54 -0300

    arrumando o layout

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -1,106 +1,113 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-import { useLayout } from '@/composables/screen';
-import CartPlus from 'vue-material-design-icons/CartPlus.vue';
-import Account from 'vue-material-design-icons/Account.vue';
-import Menu from 'vue-material-design-icons/Menu.vue';
+import { onMounted, watch } from 'vue';
+import { useProductStore } from '@/stores/product';
 
-const produtos = ref([]);
-const { browserWidth, deviceWidth, isMobile } = useScreen();
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+import Star from 'vue-material-design-icons/Star.vue';
+import { formatDescription, formatPrice, formatTitle } from '@/helpers/format';
 
-onMounted(async () => {
-  const response = await axios.get('https://fakestoreapi.com/products');
-  produtos.value = response.data;
+const props = defineProps(['category']);
+const productStore = useProductStore();
+
+async function getProducts() {
+    if (props.category) {
+        await productStore.getProductsByCategory(props.category);
+    } else {
+        await productStore.getProducts();
+    }
+}
+
+watch(() => props.category, () => {
+    getProducts();
 });
 
-const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
+
+onMounted(async () => {
+    await getProducts();
+});
+
 </script>
 
 <template>
-    <div>
-      <h1 style="color: #FD8139;">
-       Produtos - {{ browserWidth }} - {{ deviceWidth }} - {{
-      isMobile}} 
-      <span v-if="isMobile">É móvel</span>
-    </h1>
-      <div class="container">
-        <div class="card" v-for="produto in produtos" :key="produto.id">
-          <h1 class="card--title">{{ produto.title }}</h1>
-           <!--<p>{{ produto.description }}</p>-->
-           <p style="color: #FD8139;"  >{{ formatPrice(produto.price) }}</p>
-          <img class="card--avatar" :src="produto.image" :alt="produto.title" />
+    <div class="product-list">
+        <div v-if="productStore.products.length === 0">
+            <p>Produtos não encontrados</p>
         </div>
-      </div>
-      <div class="rodape-responsivo" v-if="isMobile" style="color: #FD8139;">
-          <ul class="btn-rodape">
-          <li><Account /></li>
-          <li><CartPlus /></li>
-          <li><Menu /></li>
-        </ul>
-      </div>
-      <div class="rodape-descktop" style="background-color: #FD8139; color: white; text-align: center;" v-if="browserWidth >= 768">
-        <footer>By: Laíssa Mendes</footer>
-      </div>
+        <div v-for="product in productStore.products" :key="product.id" class="product-card">
+            <div class="product-img-wrapper">
+                <img :src="product.image" alt="product.name" />
+                <heart-outline />
+            </div>
+            <div class="product-title-price">
+                <p>{{ formatTitle(product.title) }}</p>
+                <p>{{ formatPrice(product.price) }}</p>
+            </div>
+            <div class="product-description-stars">
+                <p>{{ formatDescription(product.description) }}</p>
+                <div class="stars">
+                    <star size="20" />
+                    <star size="20" />
+                    <star size="20" />
+                    <star size="20" />
+                    <star size="20" />
+                </div>
+            </div>
+        </div>
     </div>
-  </template>
+</template>
   <style scoped>
-  .container > .card > .card--title{
-    color: #FD8139;
-  }
-  .container {
+  .product-list {
     display: flex;
     flex-wrap: wrap;
-    gap: 1rem;
     justify-content: center;
-    align-items: center;
-    margin: auto;
-    padding: 1rem 0;
-  }
-  .card {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-direction: column;
-    width: 15rem;
-    height: 25rem;
-    background: #fff;
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-    border-radius: 10px;
-    margin: auto;
-    overflow: hidden;
-  }
-  .card--avatar {
-    width: 100%;
-    height: 17rem;
-    object-fit: cover;
-    margin-bottom: 0.5rem;
-  }
-  .card--title {
-    color: #222;
-    font-weight: 700;
-    text-transform: capitalize;
-    font-size: 1.1rem;
-    margin-top: 0.5rem;
-  }
-  .rodape-responsivo{
-border-radius: 10px;
-border-color: #FD8139;
-border-style: solid;
-box-shadow: 5px 5px 15px black;
-  }
-  @media (max-width: 768px) {
-  .container {
-    gap: 0.5rem;
-  }
-  .card {
-    width: 92%;
-  }
+    gap: 1.5rem;
+    padding: 1rem;
 }
 
-@media (min-width: 768px) and (max-width: 1024px) {
-  .card {
-    width: 22rem;
-  }
+.product-card {
+    width: 225px;
+    font-family: 'Belleza', sans-serif;
+
+}
+
+.product-img-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: top;
+    gap: .5rem;
+    padding-top: 20px;
+    margin-bottom: 1rem;
+    background-color: #eeeeee;
+    height: 201px;
+}
+
+.product-img-wrapper img {
+    width: 153px;
+    height: 170px;
+    object-fit: cover;
+}
+
+.product-title-price {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 1rem;
+}
+
+.product-title-price p {
+    font-weight: bold;
+    font-size: 16px;
+    color: #D5618F;
+}
+
+.product-description-stars {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 1rem;
+}
+
+.product-description-stars p {
+    font-size: 12px;
+    color: #FD8139;
 }
   </style>
